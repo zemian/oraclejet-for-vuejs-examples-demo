4ba2d5883d4d212ac505fd0b7188c41f4f93f9c9
Zemian Deng <zemian.deng@oracle.com>
2020-02-29 18:19:09 -0500

    Update demos

--- web/js/main.js
+++ web/js/main.js
@@ -7,8 +7,13 @@
 /**
  * RequireJS config for OJET
  */
-requirejs.config(
-    {
+(function () {
+    function _ojIsIE11() {
+        var nAgt = navigator.userAgent;
+        return nAgt.indexOf('MSIE') !== -1 || !!nAgt.match(/Trident.*rv:11./);
+    };
+    var _ojNeedsES5 = _ojIsIE11();
+    requirejs.config({
         baseUrl: 'js',
 
         // Path mappings for the logical module names
@@ -22,22 +27,23 @@ requirejs.config(
   "jqueryui-amd":"libs/jquery/jqueryui-amd-1.12.1",
   "promise":"libs/es6-promise/es6-promise",
   "hammerjs":"libs/hammer/hammer-2.0.8",
-  "ojdnd":"libs/dnd-polyfill/dnd-polyfill-1.0.0",
-  "persist":"libs/persist/debug",
-  "ojs":"libs/oj/v8.0.0/debug",
+  "ojdnd":"libs/dnd-polyfill/dnd-polyfill-1.0.1",
+  "ojs":"libs/oj/v8.0.0/debug" + (_ojNeedsES5 ? "_es5" : ""),
   "ojL10n":"libs/oj/v8.0.0/ojL10n",
   "ojtranslations":"libs/oj/v8.0.0/resources",
+  "persist":"libs/persist/debug",
   "text":"libs/require/text",
   "signals":"libs/js-signals/signals",
   "touchr":"libs/touchr/touchr",
-  "regenerator-runtime":"libs/@tp:regenerator-runtime@",
-  "corejs":"libs/@tp:corejs:debug@",
+  "regenerator-runtime":"libs/regenerator-runtime/runtime",
+  "corejs":"libs/corejs/shim",
   "customElements":"libs/webcomponents/custom-elements.min",
+  "proj4":"libs/proj4js/dist/proj4-src",
   "css":"libs/require-css/css",
   "css-builder":"libs/require-css/css-builder",
   "normalize":"libs/require-css/normalize"
 }
 
 // endinjector
-    }
-);
+    });
+}());
